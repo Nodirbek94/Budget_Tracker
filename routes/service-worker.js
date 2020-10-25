49d288f9ede0c52e9b91e57fba10060f1b482916
Nodirbek94 <nodirbekmaksudov@yahoo.com>
2020-10-25 18:21:03 -0400

    compressed files

---
+++ routes/service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/assets/css/style.css',
+  '/dist/index.js',
+  '/dist/db.js',
+  '/assets/images/icons/icon-192x192.png',
+  '/assets/images/icons/icon-512x512.png',
+];
